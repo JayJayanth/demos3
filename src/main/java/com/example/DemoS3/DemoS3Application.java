d04d8c630152f773fb8d59c2319af2cbde5bd032
Jayanth <jayanth.c.s@capgemini.com>
2023-02-20 15:35:22 +0530

    Hello

---
+++ src/main/java/com/example/DemoS3/DemoS3Application.java
@@ -0,0 +1,14 @@
+package com.example.DemoS3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoS3Application.class, args);
+		System.out.println("Hello World");
+	}
+
+}
